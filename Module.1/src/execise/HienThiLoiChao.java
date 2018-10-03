6b9afbe55fe413bf230d9e1c754dd147b9d4f8c3
= <=>
2018-10-03 10:46:56 +0700

    hiển thị lời chào

---
+++ Module.1/src/execise/HienThiLoiChao.java
@@ -0,0 +1,12 @@
+package execise;
+
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter You Name");
+        String name = sc.nextLine();
+        System.out.println("xin chao:"+name);
+    }
+}
