35893802b31eb2ad9dd0b5eb4e651ef948fcea43
= <=>
2018-10-03 11:59:36 +0700

    luyen tap su dung bien kieu toan tu

---
+++ Module.1/src/LuyenTapSuDungBienKieuDuLieu/Bai4.java
@@ -0,0 +1,14 @@
+package LuyenTapSuDungBienKieuDuLieu;
+
+import java.util.Scanner;
+
+public class Bai4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        int age = 2018 - year;
+        System.out.println("age =" + age);
+
+    }
+
+}
