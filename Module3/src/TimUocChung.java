12eddf66e51f0b2a87589a6ac83686a4d2ea8e58
= <=>
2018-10-06 22:39:31 +0700

    ung dung cho vay

--- Module3/src/TimUocChung.java
+++ Module3/src/TimUocChung.java
@@ -2,24 +2,20 @@ import java.util.Scanner;
 
 public class TimUocChung {
     public static void main(String[] args) {
-     int a,b;
+        int a, b;
         Scanner sc = new Scanner(System.in);
         System.out.println("nhap so a");
-       a = sc.nextInt();
+        a = sc.nextInt();
         System.out.println("nhap so b");
-       b = sc.nextInt();
-       a = Math.abs(a);
-       b = Math.abs(b);
-        if (a == 0 || b == 0)
-            System.out.println("khong co so chung lon nhat");
+        b = sc.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) System.out.println("khong co so chung lon nhat");
         while (a != b) {
-            if (a > b)
-                a = a - b;
-            else
-                b = b - a;
-            System.out.println("số chung lớn nhất="+a);
+            if (a > b) a = a - b;
+            else b = b - a;
+            System.out.println("số chung lớn nhất=" + a);
         }
-
 
 
     }
